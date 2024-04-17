f1568e96c6cae0b21fa43998a69547f1f8d5f9cd
nitheeshmk5 <22107041@srcas.ac.in>
2024-04-17 12:43:31 +0530

    Modified

--- Day08-caesarCipher/ceaser.py
+++ Day08-caesarCipher/ceaser.py
@@ -1,7 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-
 def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
   if cipher_direction == "decode":
