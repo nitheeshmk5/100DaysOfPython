d886bdf988da1bb82192950c76eca59631968c2c
nitheeshmk5 <22107041@srcas.ac.in>
2024-05-06 12:10:46 +0530

    Modified : Changed password generator

--- Day05-PasswordGenerator/password.py
+++ Day05-PasswordGenerator/password.py
@@ -1,9 +1,7 @@
 import random as rd
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to password generator !")
@@ -14,10 +12,8 @@ with_symbols = input("including symbols [Y/N] : ").upper()
 password_list = []
 
 if with_numbers == "Y":
-    #password_list.append(rd.choice(numbers))
     letters += numbers
 if with_symbols == "Y":
-    #password_list.append(rd.choice(symbols))
     letters += symbols
 
 for i in range(0,total_characters ):
@@ -25,9 +21,7 @@ for i in range(0,total_characters ):
 
 rd.shuffle(password_list)
 
-password = ""
-for i in password_list:
-    password += i
+password = "".join(password_list)
 
 print(password)
 
